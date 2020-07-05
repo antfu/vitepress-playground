d4dda6fe34f7a7e708486397721d3057ff04f776
Anthony Fu <anthonyfu117@hotmail.com>
2020-07-05 19:12:41 +0800

    ts for config

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,4 +1,7 @@
-module.exports = {
+// @ts-check
+
+/** @type {import('vitepress').UserConfig} */
+const config = {
   title: 'VitePress Playground',
 
   themeConfig: {
@@ -7,48 +10,27 @@ module.exports = {
 
     nav: [
       { text: 'Guide', link: '/guide/' },
-      { text: 'Nested', link: '/guide/nested/', },
+      { text: 'Nested', link: '/guide/nested/' },
       { text: 'API Reference', link: '/api/' },
-      { text: 'Release Notes', link: 'https://github.com/vuejs/vuex/releases' }
+      { text: 'Release Notes', link: 'https://github.com/vuejs/vuex/releases' },
     ],
 
-    sidebar: {
-      '/guide/': [
-        {
-          text: 'Prologue',
-          children: [
-            { text: 'Index', link: '/guide/' },
-            { text: 'Getting Started', link: '/guide/getting-started' },
-            { text: 'Guide Content', link: '/guide/guide-content' },
-            { text: 'Content', link: '/guide/content' }
-          ]
-        },
-        {
-          text: 'Contents',
-          children: [
-            { text: 'Content', link: '/guide/content' }
-          ]
-        }
-      ],
-
-      '/guide/nested/': [
-        {
-          text: 'Nested',
-          children: [
-            { text: 'Nested Index', link: '/guide/nested/index.md' },
-            { text: 'Nested Something', link: '/guide/nested/something.md' }
-          ]
-        }
-      ],
-
-      '/api/': [
-        {
-          text: 'Models',
-          children: [
-            { text: 'Model', link: '/api/model' }
-          ]
-        }
-      ]
-    }
-  }
+    sidebar: [
+      {
+        text: 'Prologue',
+        children: [
+          { text: 'Index', link: '/guide/' },
+          { text: 'Getting Started', link: '/guide/getting-started' },
+          { text: 'Guide Content', link: '/guide/guide-content' },
+          { text: 'Content', link: '/guide/content' },
+        ],
+      },
+      {
+        text: 'Contents',
+        children: [{ text: 'Content', link: '/guide/content' }],
+      },
+    ],
+  },
 }
+
+module.exports = config
